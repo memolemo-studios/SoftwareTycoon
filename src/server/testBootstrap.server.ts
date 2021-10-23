2d116736df5bc3786e889728c61288489a5e6d29
memothelemo <31132624+memothelemo@users.noreply.github.com>
2021-10-24 03:46:30 +0800

    Fixed issue in testBootstrap.server.ts

--- src/server/testBootstrap.server.ts
+++ src/server/testBootstrap.server.ts
@@ -2,15 +2,11 @@
 
 import { RunService } from "@rbxts/services";
 import { TestBootstrap } from "@rbxts/testez";
-import { $fileContents, $instance, $resolveFile } from "rbxts-transformer-fs";
+import { $instance, $resolveFile } from "rbxts-transformer-fs";
 
 function performBootstrap() {
-	const dotEnvFile = $fileContents("../../.env").split("\n");
-	if (dotEnvFile.findIndex(v => v === "UNIT_TEST=true") !== -1) {
-		// so that it won't actually go unit testing while in production
-		if (RunService.IsStudio()) {
-			TestBootstrap.run([$instance("src/shared/specs")]);
-		}
+	if (RunService.IsStudio()) {
+		TestBootstrap.run([$instance("src/shared/specs")]);
 	}
 }
 
