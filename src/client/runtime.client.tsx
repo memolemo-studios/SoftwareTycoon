8b85be62107d26df50dc143333d2e7349ba2c88e
memothelemo <31132624+memothelemo@users.noreply.github.com>
2021-10-25 17:34:06 +0800

    Mount Roact UI on startup

---
+++ src/client/runtime.client.tsx
@@ -0,0 +1,36 @@
+import { Flamework } from "@flamework/core";
+import Log, { Logger, LogLevel } from "@rbxts/log";
+import Roact, { mount } from "@rbxts/roact";
+import { StoreProvider } from "@rbxts/roact-rodux";
+import { Players, RunService } from "@rbxts/services";
+import GameStart from "./apps/gameStart";
+import { ClientStore } from "./store/store";
+
+Log.SetLogger(
+	Logger.configure()
+		.EnrichWithProperty("$VERSION", PKG_VERSION)
+		.SetMinLogLevel(RunService.IsStudio() ? LogLevel.Verbose : LogLevel.Information)
+		.WriteTo(Log.RobloxOutput())
+		.WriteTo(Log.RobloxOutput())
+		.Create(),
+);
+
+// mount roact ui
+Log.Info("Mounting Roact UI");
+
+const element = (
+	<StoreProvider store={ClientStore}>
+		<GameStart />
+	</StoreProvider>
+);
+
+const player_gui = Players.LocalPlayer.WaitForChild("PlayerGui");
+mount(element, player_gui, "software_tycoon_gui");
+
+// initialize flamework
+Log.Info("Initializing Flamework");
+
+Flamework.addPaths("src/client/components", "src/shared/flamework", "src/client/controllers");
+Flamework.ignite();
+
+Log.Info("Flamework is now initialized");
