e0f66699997cd2e17c87d0a566407c803d941e8a
memothelemo <31132624+memothelemo@users.noreply.github.com>
2021-10-26 12:42:38 +0800

    Trying to get rid of default loading screen

---
+++ luau/ReplicatedFirst/gameInit.client.lua
@@ -0,0 +1,6 @@
+-- roblox-ts has limitations when dealing with ReplicatedFirst
+-- which is understandable considering all assets of the game aren't loaded yet during that time ofc
+local service: ReplicatedFirst = script.Parent
+
+-- get rid of that default loading screen
+service:RemoveDefaultLoadingScreen()
