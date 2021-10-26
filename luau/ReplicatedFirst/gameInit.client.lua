55eb8248a766ee7fe2f3aaaa72f3bb0c48c3d139
memothelemo <31132624+memothelemo@users.noreply.github.com>
2021-10-26 12:56:14 +0800

    Fixed default loading screen didn't go away

--- luau/ReplicatedFirst/gameInit.client.lua
+++ luau/ReplicatedFirst/gameInit.client.lua
@@ -2,5 +2,8 @@
 -- which is understandable considering all assets of the game aren't loaded yet during that time ofc
 local service: ReplicatedFirst = script.Parent.Parent
 
+-- https://devforum.roblox.com/t/removing-the-default-loading-screen/14508/3
+game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+
 -- get rid of that default loading screen
 service:RemoveDefaultLoadingScreen()
