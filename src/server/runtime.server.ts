9e2164d6724f0adb9833c5539573d3ccfdc60291
memothelemo <31132624+memothelemo@users.noreply.github.com>
2021-10-28 03:38:27 +0800

    Embracing other flamework packages

--- src/server/runtime.server.ts
+++ src/server/runtime.server.ts
@@ -1,4 +1,16 @@
 import { Flamework } from "@flamework/core";
+import Log, { Logger, LogLevel } from "@rbxts/log";
+import { RunService } from "@rbxts/services";
 
+// logger setup
+Log.SetLogger(
+	Logger.configure()
+		.EnrichWithProperty("VERSION", PKG_VERSION)
+		.SetMinLogLevel(RunService.IsStudio() ? LogLevel.Verbose : LogLevel.Information)
+		.WriteTo(Log.RobloxOutput())
+		.Create(),
+);
+
+// initialize flamework
 Flamework.addPaths("src/server/services", "src/shared/flamework");
 Flamework.ignite();
