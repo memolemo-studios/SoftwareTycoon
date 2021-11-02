bd1a50b6843935fd94a31ae00b5298ada8ecabec
memothelemo <31132624+memothelemo@users.noreply.github.com>
2021-11-03 04:14:02 +0800

    Useless commit

--- src/shared/util/cframe.ts
+++ src/shared/util/cframe.ts
@@ -7,6 +7,15 @@ namespace CFrameUtil {
 	export function fromPositionAndRotation({ X, Y, Z }: Vector3, { X: RX, Y: RY, Z: RZ }: Vector3) {
 		return new CFrame(X, Y, Z).mul(CFrame.Angles(RX, RY, RZ));
 	}
+
+	/**
+	 * Converts CFrame into Vector3
+	 * @param cframe CFrame to convert to
+	 */
+	export function convertToVector(cf: CFrame) {
+		const [rx, ry, rz] = cf.ToEulerAnglesXYZ();
+		return [cf.Position, new Vector3(rx, ry, rz)] as const;
+	}
 }
 
 export = CFrameUtil;
