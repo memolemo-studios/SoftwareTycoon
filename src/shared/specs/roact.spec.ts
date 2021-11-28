416a4311c020430f217cdcf390f158623c8da0e5
memothelemo <31132624+memothelemo@users.noreply.github.com>
2021-11-29 04:50:05 +0800

    Add additional RoactUtil unit tests

--- src/shared/specs/roact.spec.ts
+++ src/shared/specs/roact.spec.ts
@@ -1,5 +1,6 @@
 /// <reference types="@rbxts/testez/globals" />
 
+import { Instant, SingleMotor } from "@rbxts/flipper";
 import Roact from "@rbxts/roact";
 import { RoactUtil } from "shared/utils/roact";
 
@@ -12,6 +13,38 @@ export = () => {
 
     it("must returns false if it is not Roact binding", () => {
       expect(RoactUtil.isBinding("123")).be.equal(false);
+    });
+  });
+
+  describe("RoactUtil.getBindableValue", () => {
+    const bindable = Roact.createBinding(10);
+    const non_bindable = 20;
+
+    it("should get the value if it is a Roact binding", () => {
+      const value = RoactUtil.getBindableValue(bindable);
+      expect(value).be.equal(10);
+    });
+
+    it("should get the value if it is a non-Roact binding", () => {
+      const value = RoactUtil.getBindableValue(non_bindable);
+      expect(value).be.equal(20);
+    });
+  });
+
+  describe("RoactUtil.makeBindingFromMotor", () => {
+    it("should create binding from motor", () => {
+      const motor = new SingleMotor(0);
+      const binding = RoactUtil.makeBindingFromMotor(motor);
+
+      // binding test
+      expect(RoactUtil.isBinding(binding)).be.equal(true);
+      expect(RoactUtil.getBindableValue(binding)).be.equal(0);
+
+      // if the binding can keep up with the motor
+      motor.setGoal(new Instant(1));
+      motor.step(1);
+
+      expect(RoactUtil.getBindableValue(binding)).be.equal(1);
     });
   });
 };
