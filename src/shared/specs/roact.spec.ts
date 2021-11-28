744db7797bf95b3f07166b2d19a83c5049e63d37
memothelemo <31132624+memothelemo@users.noreply.github.com>
2021-11-29 04:54:03 +0800

    Fix some issues when unit testing

--- src/shared/specs/roact.spec.ts
+++ src/shared/specs/roact.spec.ts
@@ -17,34 +17,19 @@ export = () => {
   });
 
   describe("RoactUtil.getBindableValue", () => {
-    const bindable = Roact.createBinding(10);
+    const [bindable, set_bindable] = Roact.createBinding(10);
     const non_bindable = 20;
 
     it("should get the value if it is a Roact binding", () => {
-      const value = RoactUtil.getBindableValue(bindable);
-      expect(value).be.equal(10);
+      expect(RoactUtil.getBindableValue(bindable)).be.equal(10);
+
+      set_bindable(20);
+      expect(RoactUtil.getBindableValue(bindable)).be.equal(20);
     });
 
     it("should get the value if it is a non-Roact binding", () => {
       const value = RoactUtil.getBindableValue(non_bindable);
       expect(value).be.equal(20);
-    });
-  });
-
-  describe("RoactUtil.makeBindingFromMotor", () => {
-    it("should create binding from motor", () => {
-      const motor = new SingleMotor(0);
-      const binding = RoactUtil.makeBindingFromMotor(motor);
-
-      // binding test
-      expect(RoactUtil.isBinding(binding)).be.equal(true);
-      expect(RoactUtil.getBindableValue(binding)).be.equal(0);
-
-      // if the binding can keep up with the motor
-      motor.setGoal(new Instant(1));
-      motor.step(1);
-
-      expect(RoactUtil.getBindableValue(binding)).be.equal(1);
     });
   });
 };
