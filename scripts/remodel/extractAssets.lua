7355ca06792f6cfd7d52aed0e3458c078d96ab06
memothelemo <31132624+memothelemo@users.noreply.github.com>
2021-11-29 02:29:27 +0800

    Rework on the extraction script

--- scripts/remodel/extractAssets.lua
+++ scripts/remodel/extractAssets.lua
@@ -13,10 +13,31 @@ local excluded_classes = {
 -- Only extract services when it needs to in the table
 local services = {
 	"Workspace",
-	"Lighting"
+	"Lighting",
+	"ReplicatedStorage",
 }
 
--- IsA method does not exists in Remodel
+-- Excluded instances
+local excluded_instances = {
+	"ReplicatedStorage.Game",
+	"ReplicatedStorage.rbxts_include"
+}
+
+local function log(message, ...)
+	print(string.format(message, ...))
+end
+
+-- Alternative to table.find
+local function find(tbl, value)
+	for i, v in ipairs(tbl) do
+		if v == value then
+			return i, v
+		end
+	end
+	return nil, nil
+end
+
+-- IsA method does not exists in Remodel but I have to use base classname
 local function isObjectExcluded(object)
 	for _, class_name in ipairs(excluded_classes) do
 		if object.ClassName == class_name then
@@ -30,22 +51,25 @@ end
 function extractFile(object, location)
 	-- Make sure that object is not a BaseScript
 	if isObjectExcluded(object) then return end
-
-	local shouldBeDirectory = object.ClassName == "Folder"
-	print("Extracting " .. (shouldBeDirectory and "directory" or "file") .. ": " .. location)
+	local is_directory = object.ClassName == "Folder"
 
 	-- Folders should be considered as a directory
-	if shouldBeDirectory then
+	if is_directory then
 		return extractDirectory(object, location .. "/")
 	end
 
 	-- Extraction process!
+	log("Extracting file: %s", location)
 	remodel.writeModelFile(object, location .. ".rbxmx")
 end
 
 -- Extracts any folder to a directory
 function extractDirectory(object, location)
+	-- If that object is in the excluded list, do not include it
+	if find(excluded_instances, object:GetFullName()) ~= nil then return end
+
 	-- Make a new directory
+	log("Extracting directory: %s", location)
 	remodel.createDirAll(location)
 
 	-- Extract object's children
