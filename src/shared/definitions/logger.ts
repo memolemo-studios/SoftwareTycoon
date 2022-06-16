8b891856182d594844dd15457c9861495b483a15
memothelemo <memothelemo@protonmail.com>
2022-06-17 04:10:54 +0800

    feat: custom log sink
    It allows to print the following format:
    ```
    [INF: MyService] Hello world!
    [DBG: AnotherService] Yes...
    ```
    
    Instead of using the default from @rbxts/log (LogEventRobloxOutputSink):
    ```
    [INF] Hello world!
    [DBG] Yes...
    ```
    
    It's long but it preserves the source context and allows for easier
    debugging later on.

--- src/shared/definitions/logger.ts
+++ src/shared/definitions/logger.ts
@@ -1,6 +1,46 @@
-import Log, { Logger, LogLevel } from "@rbxts/log";
+import { Logger, LogLevel } from "@rbxts/log";
+import { ILogEventSink, LogEvent } from "@rbxts/log/out/Core";
+import { MessageTemplateParser, PlainTextMessageTemplateRenderer } from "@rbxts/message-templates";
 import { RunService } from "@rbxts/services";
 import { isGameInProduction } from "shared/utils/game";
+
+class SoftwareTycoonSink implements ILogEventSink {
+  public Emit(message: LogEvent): void {
+    const template = new PlainTextMessageTemplateRenderer(
+      MessageTemplateParser.GetTokens(message.Template),
+    );
+    let tag: string;
+    switch (message.Level) {
+      case LogLevel.Verbose:
+        tag = "VRB";
+        break;
+      case LogLevel.Debugging:
+        tag = "DBG";
+        break;
+      case LogLevel.Information:
+        tag = "INF";
+        break;
+      case LogLevel.Warning:
+        tag = "WRN";
+        break;
+      case LogLevel.Error:
+        tag = "ERR";
+        break;
+      case LogLevel.Fatal:
+        tag = "FLT";
+        break;
+    }
+
+    const messageRendered = template.Render(message);
+    const formattedMessage = `[${tag}: ${message.SourceContext ?? "Game"}] ${messageRendered}`;
+
+    if (message.Level >= LogLevel.Warning) {
+      warn(formattedMessage);
+    } else {
+      print(formattedMessage);
+    }
+  }
+}
 
 /**
  * The default configuration for this game.
@@ -13,4 +53,4 @@ export const LogConfig = Logger.configure()
   .EnrichWithProperty("SessionEnv", isGameInProduction() ? "debug" : "production")
   .EnrichWithProperty("SessionSide", RunService.IsClient() ? "client" : "server")
   .SetMinLogLevel(isGameInProduction() ? LogLevel.Information : LogLevel.Verbose)
-  .WriteTo(Log.RobloxOutput());
+  .WriteTo(new SoftwareTycoonSink());
